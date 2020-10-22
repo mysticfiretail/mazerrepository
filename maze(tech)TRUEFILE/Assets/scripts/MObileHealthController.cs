a27a3fedb062ab8442dfb7cd5f855e00a78723ef
KITTY\Catherine <cat@ovsy.com>
2020-10-22 12:12:10 -0700

    healthcontroller

---
+++ maze(tech)TRUEFILE/Assets/scripts/MObileHealthController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MObileHealthController : MonoBehaviour
+{
+    public float playerHealth;
+
+    [SerializeField] private Text HealthText;
+
+    private void Start()
+    {
+        UpdateHealth();
+    }
+    public void UpdateHealth()
+    {
+        HealthText.text = playerHealth.ToString("0");
+    }
+
+}
---
+++ maze(tech)TRUEFILE/Assets/scripts/MobileDamageController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileDamageController : MonoBehaviour
+{
+    [SerializeField] private float vinesDamage;
+    [SerializeField] private MObileHealthController healthController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Damage();
+        }
+    }
+
+
+    void Damage()
+    {
+        healthController.playerHealth = healthController.playerHealth - vinesDamage;
+        healthController.UpdateHealth();
+        this.gameObject.SetActive(false);
+    }
+}
