0d7919cc32470b6609dd8a17c2d28b4cdd35d571
KITTY\Catherine <cat@ovsy.com>
2020-10-27 11:42:55 -0700

    new version **

---
+++ maze(tech)TRUEFILE/Assets/scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{      
+    public bool IsLevelComplete()
+        {
+            Box[] boxes = FindObjectsOfType<Box>();
+            foreach(var box in boxes)
+            {
+                if (!box.m_OnCross)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsLevelComplete();
+    }
+}
